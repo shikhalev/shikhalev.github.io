01d13a827ab8fb4ae7297278e33fee3e7b63c4ff
Ivan Shikhalev <shikhalev@gmail.com>
2021-03-29 17:50:36 +0500

    minor fix

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,5 +1,6 @@
 (function () {
-  const TOP_LEVEL_COMMENT_PROMPT = 'Будет добавлен комментарий верхнего уровня.';
+  const TOP_LEVEL_COMMENT_PROMPT =
+    "Будет добавлен комментарий верхнего уровня.";
 
   const PATH = "/admin/last-comments.json";
   const INTERVAL = 2000;
@@ -16,14 +17,16 @@
 
   function process(data) {
     data.forEach((comment) => {
-      let parent;
-      if (comment.parent_id && comment.parent_id != "") {
-        parent = document.getElementById(comment.parent_id);
-      } else {
-        parent = document.getElementById("comments");
-      }
-      if (parent) {
-        parent.appendChild(make(comment.content));
+      if (comment.comment_id && comment.comment_id != "") {
+        let parent;
+        if (comment.parent_id && comment.parent_id != "") {
+          parent = document.getElementById(comment.parent_id);
+        } else {
+          parent = document.getElementById("comments");
+        }
+        if (parent) {
+          parent.appendChild(make(comment.content));
+        }
       }
     });
   }
@@ -48,32 +51,34 @@
   function reset_answer(e) {
     let header = document.getElementById("comment-form-header");
     let field = document.getElementById("parent_id");
-    header.innerHTML = 'Будет добавлен комментарий верхнего уровня';
-    field.value = '';
+    header.innerHTML = "Будет добавлен комментарий верхнего уровня";
+    field.value = "";
   }
 
   function answer_action(e) {
     let target = e.target;
     let action = target.dataset.action;
     let comment_id = target.dataset.comment;
-    let header = document.getElementById('comment-form-header');
-    let field = document.getElementById('parent_id');
+    let header = document.getElementById("comment-form-header");
+    let field = document.getElementById("parent_id");
     header.innerHTML = `Будет добавлен ответ на комментарий <a href="#${comment_id}">#${comment_id}</a> [<a id="action-reset" style="cursor: pointer;" data-action="reset">сбросить</a>]`;
     field.value = comment_id;
 
     let message = document.getElementById("comment_message");
-    if (action == 'cite') {
-      let source = document.getElementById(comment_id).getElementsByClassName('comment-message')[0];
-      let source_lines = source.textContent.trim().split('\n');
+    if (action == "cite") {
+      let source = document
+        .getElementById(comment_id)
+        .getElementsByClassName("comment-message")[0];
+      let source_lines = source.textContent.trim().split("\n");
       for (let line of source_lines) {
-        message.value += '\r\n> ' + line.trim();
+        message.value += "\r\n> " + line.trim();
       }
       message.value += "\r\n";
     }
     message.scrollIntoView({ block: "nearest", inline: "nearest" });
 
-    let reset_a = document.getElementById('action-reset');
-    reset_a.addEventListener('click', reset_answer);
+    let reset_a = document.getElementById("action-reset");
+    reset_a.addEventListener("click", reset_answer);
   }
 
   function init_answers() {
@@ -81,16 +86,18 @@
     for (let bar of ans_bars) {
       bar.style.display = "block";
       for (let el of bar.children) {
-        if (el.dataset.action == 'answer' || el.dataset.action == 'cite') {
-          el.addEventListener('click', answer_action);
+        if (el.dataset.action == "answer" || el.dataset.action == "cite") {
+          el.addEventListener("click", answer_action);
         }
       }
     }
   }
 
   function init_id() {
-    var new_id = Math.floor(Math.random() * 16 * 16 * 16 * 16 * 16 * 16).toString(16);
-    var field = document.getElementById('comment_id');
+    var new_id = Math.floor(
+      Math.random() * 16 * 16 * 16 * 16 * 16 * 16
+    ).toString(16);
+    var field = document.getElementById("comment_id");
     field.value = new_id;
   }
 
