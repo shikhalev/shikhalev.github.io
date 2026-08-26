6eb40bfbd410a0180713045b4c9e480933fe9d13
shikhalev <shikhalev@users.noreply.github.com>
2026-08-26 04:30:08 +0000

    Publishing by Jekyll Build @ Wed Aug 26 04:30:08 UTC 2026

--- js/is-images-0.8.2.4.js
+++
@@ -1,7 +0,0 @@
-import { initSlidesWheel, initSlidesNavBar, initViewBox } from '/js/plugins/is-images-0.8.2.4.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initSlidesWheel();
-  initSlidesNavBar();
-  initViewBox();
-});
---
+++ js/is-images-0.8.2.6.js
@@ -0,0 +1,7 @@
+import { initSlidesWheel, initSlidesNavBar, initViewBox } from '/js/plugins/is-images-0.8.2.6.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSlidesWheel();
+  initSlidesNavBar();
+  initViewBox();
+});
